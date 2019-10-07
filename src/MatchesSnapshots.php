78ee00ebe76174d0b4f51c3e17545a2913b1e76f
Thomas van der Veen <thomas@owow.io>
2019-10-07 13:36:56 +0200

    MatchesSnapshots trait and JsonStructureDriver class.

---
+++ src/MatchesSnapshots.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OwowAgency\Snapshots\Library\Snapshots;
+
+use Illuminate\Foundation\Testing\TestResponse;
+use OwowAgency\Snapshots\Library\Snapshots\Drivers\JsonStructureDriver;
+use Spatie\Snapshots\MatchesSnapshots as BaseMatchesSnapshots;
+
+trait MatchesSnapshots
+{
+    use BaseMatchesSnapshots;
+
+    /**
+     * Asserts the structure of the response json with a snapshot.
+     *
+     * @param  \Illuminate\Foundation\Testing\TestResponse  $response
+     * @return void
+     */
+    public function assertJsonStructureSnapshot(TestResponse $response): void
+    {
+        $this->assertMatchesSnapshot(
+            $response->json(),
+            new JsonStructureDriver()
+        );
+    }
+}
